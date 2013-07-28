5aca2e1b9216f6fff51b01b2134b559f2621f947
Jonathan DePrizio <nemilar@gmail.com>
2013-07-28 17:52:18 -0400

    Cleanup and comments; also start prepping for RFID reader.

--- arduino/code/code.ino
+++ arduino/code/code.ino
@@ -1,52 +1,90 @@
 #include <SoftwareSerial.h>
 #include <string.h>
 
-const int BACKLIGHT_OFF=18;
-const int BACKLIGHT_ON=17;
 
-const int audibleIndicatorLED = 4 ;
+// Some basic shortcut-type stuff.
+// For the LCD display:
+const int BACKLIGHT_OFF =  18;
+const int BACKLIGHT_ON  =  17;
+const int LCD_CLEAR     =  12;
+const int ALARM_TONE    =  223; // The tone to play when the alarm is tripped
+// See http://www.parallax.com/Portals/0/Downloads/docs/prod/displays/27979-Parallax-Serial-LCDs-Product-Guide-v3.1.pdf
+// For more information including available tones to play on trip.  They're all pretty obnoxious.
+const int LCD_ALARM     = 1; 
+const int LCD_READY     = 0;
+
+// For the RFID 
+#define RFID_LEGACY        0x0F
+#define RFID_READ          0x01
+#define RFID_WRITE         0x02
+#define RFID_RESET         0x06
+const int MAX_RFID_SLEEP = 750; // Time to wait for the RFID to respond (ms)
+
+
+// Pin assignments
+// Digital pins - Output pins
+const int audibleIndicatorLED = 4; 
 const int tripIndicatorLED = 3;
 const int LCDPin = 7;
-const int SensorTrip = 6;
+const int RFID_Out = 11;
+//Digital pins - Input pins
+const int SensorTrip = 6; // PIR 
 const int s1 = 5; // switch 1 - audible alarm
 const int s2 = 2; // switch 2 - send email
-const int blinkDuration = 1000; // how long to blink after detection of motion; ms
+const int RFID_In = 10;
 
+
+// Other stuff - User defined
+const int blinkDuration =                 1000;// how long to blink after detection of motion; ms
+const int tripIndicatorLEDBlinkInterval = 100; // blinking interval; ms
+                                               // Note that this controls the # of blinks
+
+
+// Working variables.
 int tripLIndicatorLEDState = 0;
-long   lastBlink;
-int tripIndicatorLEDBlinkInterval = 100; // blinking interval; ms
+int LCDState = LCD_READY;
+
+long  lastBlink;
 long tripIndicatorLEDBlinkUntil = 0; 
 int audibleAlarm = 0;
 char* message;
 byte oldState;
 
-SoftwareSerial mySerial = SoftwareSerial(255,LCDPin);
+SoftwareSerial lcdInterface = SoftwareSerial(255,LCDPin);
 
 void setup()
 {
     Serial.begin(9600);
     Serial.println("System Live...");
-    pinMode(LCDPin, OUTPUT); // Set Digitalpin 4 to an output pin
     pinMode(SensorTrip, INPUT); // readings from the PIR 
     pinMode(s1, INPUT); // switch toggle 1 - audible alarm switch
     pinMode(s2, INPUT); // switch toggle 2 - send email 
+    pinMode(RFID_In, INPUT);
     
+    pinMode(RFID_Out, OUTPUT);
+    pinMode(LCDPin, OUTPUT); // Set Digitalpin 4 to an output pin
     pinMode(audibleIndicatorLED, OUTPUT); // LED indicating whether the alarm will sound
-    pinMode(tripIndicatorLED, OUTPUT);
-    digitalWrite(LCDPin, HIGH); // enabled
+    pinMode(tripIndicatorLED, OUTPUT); // Indicates a trip of the sensor
+    
+    digitalWrite(LCDPin, HIGH); // Turn on the LCD to start
     
     
-    mySerial.begin(9600);
-    mySerial.write(213);  // set tone to 1/2 note (1sec)
-    //mySerial.write(219); // set to MAXIMUM ANNOYANCE!!
-    mySerial.write(12); // clear
-    mySerial.write(BACKLIGHT_ON); //backlight on
+    lcdInterface.begin(9600);
+    lcdInterface.write(213);  // set tone to 1/2 note (1sec)
+    //lcdInterface.write(219); // set to MAXIMUM ANNOYANCE!!
+    lcdInterface.write(12); // clear
+    lcdInterface.write(BACKLIGHT_ON); //backlight on
     delay(5); // required, for some reason...
 }
 
 void loop()
 {
-      // time tracking for LED blinking
+
+      /* This section controls the blinking of the tripIndicatorLED.  This LED blinks
+      whenever the sensor is tripped (when SensorTrip is high).  When tripped, the 
+      tripIndicatorLEDBlinkUntil is set to some future time.  The following code checks
+      whether this is the case, and if so, blinks accordingly (with some additional logic
+      to control the frequency of the blinking */
       if (tripIndicatorLEDBlinkUntil > millis())
       {
         // we are in a blinking state.
@@ -65,18 +103,26 @@ void loop()
            } 
            lastBlink = millis();
         }
-        mySerial.write(BACKLIGHT_ON);
+        lcdInterface.write(BACKLIGHT_ON);
       }
       else
       {
         digitalWrite(tripIndicatorLED, LOW);
-      //       Serial.write("Out of blinking state.  Everyone off.");
-        mySerial.write(BACKLIGHT_OFF);
-      
+        /* If the LCD is currently showing that we are alarmed, then
+        clear it.  Otherwise, ignore. */
+        if (LCDState == LCD_ALARM)
+        {
+          lcdInterface.write(BACKLIGHT_OFF);
+          lcdInterface.write(LCD_CLEAR);
+          lcdInterface.write("Alarm system    activated.");
+          LCDState = LCD_READY;
+        }
       }
       
-      // If switch1 is on, we'll beep.  This setting is indicated
-     // by an LED on audibleInicatorLED (yellow).
+     /* Check if switch 1 is set to ON.  This tells us whether or not we 
+     should beep audibly on a SensorTrip.  It's useful to have this, since 
+     when you're building and debugging the system, you'll be tripping the 
+     sensor a lot, and it can be incredibly annoying. */
      
      if (digitalRead(s1) == HIGH)
      {
@@ -90,40 +136,38 @@ void loop()
        digitalWrite(audibleIndicatorLED, LOW);
      }
      
-     // When the PIR is tripped, do this:  * ACTION *     
-     //
-     //
-     ///////////////////////////////////////////////////////
+     
+     
+     /* Code that is executed when the sensor actually trips (SensorTrip reads HIGH). */
+     /*********************************************************************************/
      if (digitalRead(SensorTrip) == HIGH && oldState == LOW)
      {
-      mySerial.write(12);
-      mySerial.write(BACKLIGHT_ON);
+      lcdInterface.write(LCD_CLEAR);
+      lcdInterface.write(BACKLIGHT_ON);
+      lcdInterface.write("ALARM TRIPPED!  ");
+      LCDState = LCD_ALARM;
       
-      
-      //mySerial.write("Backlight Off");
       if (audibleAlarm == 1)
       {
-        mySerial.write(223);
+        lcdInterface.write(ALARM_TONE); 
       }
       oldState = HIGH;
       if (digitalRead(s2) == HIGH)
        {
          // switch two indicates that we need to send an email.
-         // the pi reads thins string and sends it.
+         // the pi reads this string and sends it.
          Serial.println("Motion detected!");
+         lcdInterface.write("EMAIL/PHOTO SENT");
        }
-      
 
       tripIndicatorLEDBlinkUntil = millis() + blinkDuration;
-      
       
      }
      
      else if (digitalRead(SensorTrip) == HIGH && oldState == HIGH)
      {
        oldState = HIGH;
-
-        tripIndicatorLEDBlinkUntil = millis() + blinkDuration;
+       tripIndicatorLEDBlinkUntil = millis() + blinkDuration;
      }
      else if (digitalRead(SensorTrip) == LOW && oldState == LOW)
      {
@@ -131,9 +175,7 @@ void loop()
      }
      else if (digitalRead(SensorTrip) == LOW && oldState == HIGH)
      {
-       mySerial.write(12);
-       mySerial.write(BACKLIGHT_OFF);
-       //mySerial.write("Backlight Off");
+
        oldState = LOW;
   
      }
